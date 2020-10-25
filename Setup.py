8051acc96f47cdeb9db1683497b9a46c83c9de7e
tssytrack <ts01561526@gmail.com>
2020-10-24 22:20:31 -0400

    remove some code

--- Setup.py
+++ Setup.py
@@ -1,14 +1,4 @@
-import math
-import os
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
 
-print("haha")
-print("fuck")
-
-name = input("Your name? ")
-print("Hello, ", name)
+print("help")
