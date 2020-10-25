77e0bd4d1c083aab252f1093eb1b7e380e0db2aa
tssytrack <ts01561526@gmail.com>
2020-10-24 22:18:29 -0400

    Initial commit

---
+++ Setup.py
@@ -0,0 +1,14 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+print("haha")
+print("fuck")
+
+name = input("Your name? ")
+print("Hello, ", name)
